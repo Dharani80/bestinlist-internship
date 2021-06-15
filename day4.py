6aecd4db4820392c0825da7237cc1f5629d52aff
Dharani80 <85792811+Dharani80@users.noreply.github.com>
2021-06-15 18:23:03 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,58 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=b=c=30
+>>> print(a/10)
+3.0
+>>> print(b*50)
+1500
+>>> print(c+60)
+90
+>>> X=Birds
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    X=Birds
+NameError: name 'Birds' is not defined
+>>> X="Birds"
+>>> print(X)
+Birds
+>>> print(X.replace("d","l"))
+Birls
+>>> X="Birls"
+>>> print(X)
+Birls
+>>> Print(X.replace("B","G"))
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    Print(X.replace("B","G"))
+NameError: name 'Print' is not defined
+>>> print(X.replace("B","G"))
+Girls
+>>> #CONVERSING OF INTEGERS TO FLOAT
+>>> #DECLARIND INT
+>>> M=12
+>>> print(M)
+12
+>>> print(type(M))
+<class 'int'>
+>>> print("M is",type(M))
+M is <class 'int'>
+>>> M=float(M)
+>>> print(M)
+12.0
+>>> print(type(M))
+<class 'float'>
+>>> #converting float into int data type
+>>> #Declaring float
+>>> R=12.0
+>>> print(R)
+12.0
+>>> print(type(R))
+<class 'float'>
+>>> print("R is",type(R))
+R is <class 'float'>
+>>> R=int(R)
+>>> print(R)
+12
+>>> print(type(R))
+<class 'int'>
+>>> 
