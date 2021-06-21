7c3d0d92864e814d6600734bc254d82f6716b3aa
Dharani80 <85792811+Dharani80@users.noreply.github.com>
2021-06-21 12:50:41 +0530

    Add files via upload

---
+++ day8 mean.py
@@ -0,0 +1,11 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> n_num = [1,2,3,4,5]
+>>> n = len(n_num)
+>>> get_sum = sum(n_num)
+>>> mean = get_sum/n
+>>> print("mean/average is:"+str(mean))
+mean/average is:3.0
+>>> 0
+0
+>>> 
