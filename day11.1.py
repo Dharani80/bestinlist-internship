79c91d530eb2f20addd0355065960afea39e9ece
Dharani80 <85792811+Dharani80@users.noreply.github.com>
2021-06-24 17:35:41 +0530

    Add files via upload

---
+++ day11.1.py
@@ -0,0 +1,11 @@
+import scratch_13
+scratch_13.mymodule_13('mag')
+scratch_13.l[1]=8
+print|("After updating:",scratch_13)
+import random
+print(random.randiant(1,100))
+print(random.random())
+
+import sys
+sys.path.append('c:/users/dharani/downloads')
+print(sys.path)
---
+++ day11.3.py
@@ -0,0 +1,11 @@
+import scratch_13
+scratch_13.mymodule('mag')
+scratch_13.1[1]=8
+print("After updating:",scratch_13.1)
+import random
+print(random.randint(1,100))
+print(random.random())
+
+import sys
+sys.path.append('c:/users/srihitha/downloads')
+print(sys.path)
