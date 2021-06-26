96176e81e21e783468af7340b7e92e51894699d5
Dharani80 <85792811+Dharani80@users.noreply.github.com>
2021-06-26 19:32:00 +0530

    Add files via upload

---
+++ day12.py
@@ -0,0 +1,7 @@
+#creating a file
+file1 = open("30days 30 hour operation","w")
+X = [ "I have completed 10 days successfully \n"]
+#appending a file
+file1 = open("30 days 30 hour operation","a")
+file1.write("My name is Dharani \n")
+file1.close()
