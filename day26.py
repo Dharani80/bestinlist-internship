60eaf89a7794a990d8599d917eb7190e34c14053
Dharani80 <85792811+Dharani80@users.noreply.github.com>
2021-07-25 15:56:01 +0530

    Add files via upload

---
+++ day26.py
@@ -0,0 +1,9 @@
+#1Q
+list1 = ['srihitha','sri','rama','seeta']
+print(list(enumerate(list1)))
+print(enumerate(list1))
+
+#2Q
+tuple1=['srihitha','sri','rama','seeta']
+print(tuple(enumerate(tuple1)))
+© 2021 GitHub, Inc.
